20c629dca096812ade66cb3a160857370a7bd150
Adam Hammond <ahammond@harding.edu>
2016-04-08 16:21:08 -0500

    Clear

--- cloud/app/routes/index.routes.js
+++
@@ -1,6 +0,0 @@
-var Authorization = require("../authorization");
-
-module.exports = function(app) {
-    var index = require("../controllers/index.controller");
-    app.get("/", Authorization.BasicAuthentication, index.render);
-}
--- cloud/app/views/unauthorized.hb
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-You are NOT authorized.
-</body>
-</html>
